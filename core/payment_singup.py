88a049e79e5e6998e4be3c217216ffe1d04a1f9e
Proladon <proladon@gmail.com>
2019-09-22 15:50:12 +0800

    json

--- core/payment_singup.py
+++ core/payment_singup.py
@@ -1,30 +1,33 @@
 import pygetwindow as gw
 import pyautogui as auto
+import json
 
+with open('setting.json', 'r', encoding='utf8') as jfile:
+	jdata = json.load(jfile)
 
-def move_click(x, y, s= None):
-    auto.moveTo(x, y, s)
-    auto.click()
+def move_click(data):
+	auto.moveTo(data[0], data[1], data[2])
+	auto.click()
 
 class Payment:
-    def payment_signup(self):
-        #點擊支付證明採購
-        move_click(106, 148, 0.2)
-        #點擊支付證明申請
-        move_click(106, 170, 0.2)
-        #點擊新增
-        move_click(230, 104, 0.5)
+	def payment_signup(self):
+		data = jdata['支付證明申請']
+		#點擊支付證明採購
+		move_click(data['點擊支付證明採購'])
+		#點擊支付證明申請
+		move_click(data['點擊支付證明申請'])
+		#點擊新增
+		move_click(data['點擊新增'])
 
-    def budget_source(self, win):
-        source_list = "1.學校經費\n2.國科會\n3.大同松公司\n4.教育部(專案計畫)"
-        print(source_list)
-        source = input('選擇預算來源(代號): ')
-        checker = True
-        win
-        
-        while checker == True:
-            if source == '1':
-                move_click(700, 265, 0.2)
-                move_click(650, 290, 0.2)
-                checker = False
-            
+	def budget_source(self, win):
+		source_list = "1.學校經費\n2.國科會\n3.大同松公司\n4.教育部(專案計畫)"
+		print(source_list)
+		source = input('選擇預算來源(代號): ')
+		checker = True
+		
+		# while checker == True:
+		# 	if source == '1':
+		# 		move_click(700, 265, 0.2)
+		# 		move_click(650, 290, 0.2)
+		# 		checker = False
+			
--- main.py
+++ main.py
@@ -1,28 +1,30 @@
 from core.payment_singup import Payment
 import pygetwindow as gw
 import pyautogui as auto
+import json
 
-class Main (Payment):
-    def __init__(self):
-        self.toggle = True    
+class Main ():
+	def __init__(self):
+		self.toggle = True    
 
-    # 啟動與放大報帳頁面
-    def get_win(self):
-        
-        while self.toggle == True:
-            try:
-                self.win = gw.getWindowsWithTitle('總務採購系統')[0]
-                self.win.activate()
-                self.win.maximize()
-                self.toggle = False
-            except IndexError:
-                print('請先將遊覽器頁面切換到 "總務採購系統"。')
-                input('按任意鍵繼續...')
+	# 啟動與放大報帳頁面
+	def get_win(self):
+		
+		while self.toggle == True:
+			try:
+				self.win = gw.getWindowsWithTitle('總務採購系統')[0]
+				self.win.activate()
+				self.win.maximize()
+				self.toggle = False
+			except IndexError:
+				print('請先將遊覽器頁面切換到 "總務採購系統"。')
+				input('按任意鍵繼續...')
 
 
 if __name__ == "__main__":
-    Main = Main()
-    Main.get_win()
+	Main = Main()
+	Main.get_win()
 
-    Main.payment_signup()
-    Main.budget_source(Main.get_win())
+	Payment = Payment()
+	Payment.payment_signup()
+	Payment.budget_source(Main.get_win())
